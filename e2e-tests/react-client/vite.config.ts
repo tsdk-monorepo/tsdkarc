1f2d8f8c5a0f7ec4673db773f6c5049c430fad1a
suhaotian <h4av.one@gmail.com>
2026-08-05 15:29:34 +1000

    remove

--- e2e-tests/react-client/vite.config.ts
+++ e2e-tests/react-client/vite.config.ts
@@ -1,12 +1,16 @@
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
 import tailwindcss from "@tailwindcss/vite";
+import vue from "@vitejs/plugin-vue";
 
 // https://vite.dev/config/
 export default defineConfig({
-  plugins: [react(), tailwindcss()],
+  optimizeDeps: {
+    exclude: ["tsdkarc-x", "tsdkarc"],
+  },
+  plugins: [react(), vue(), tailwindcss()],
   resolve: {
     // Forces Vite to always resolve these libraries to the root node_modules version
-    dedupe: ["react", "react-dom"],
+    dedupe: ["react", "react-dom", "tsdkarc", "tsdkarc-x", "vue"],
   },
 });
